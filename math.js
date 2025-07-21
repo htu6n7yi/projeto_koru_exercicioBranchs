d5da370627faf78457429c36a696896efb369af2
RenataARocha <programadorawebrenatarocha@gmail.com>
2025-07-21 16:10:05 -0300

    feat: converte string '123.45' para número no desafio 3b do math.js

---
+++ math.js
@@ -0,0 +1,5 @@
+const stringNumero = "123.45";
+const numeroConvertido = parseFloat(stringNumero);
+
+console.log("Número convertido:", numeroConvertido);
+console.log("Tipo de dado:", typeof numeroConvertido);
