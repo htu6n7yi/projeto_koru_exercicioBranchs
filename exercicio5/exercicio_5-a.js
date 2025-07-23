9c85414f1e5ae63f3ab33633c68b23599bd89d37
htu6n7yi <jcavalcanti008@gmail.com>
2025-07-23 16:57:35 -0300

    colocando os itens de maneira organizada na main

---
+++ exercicio5/exercicio_5-a.js
@@ -0,0 +1,2 @@
+ for (let i = 0; i <= 10; i++) {
+    console.log("7 x " + i  + " = " + (i * 7)); }
---
+++ exercicio5/exercicio_5-b.js
@@ -0,0 +1,10 @@
+let numeros = new Array(78, 37, 89, 2, 487, 3, 9)
+let soma = 0;
+
+for (let i = 0; i< numeros.length; i++){
+    
+    soma += numeros[i];
+
+//console.log feito dentro do loop para mostrar a soma a cada repetição
+    console.log("Soma: ", soma);
+}
